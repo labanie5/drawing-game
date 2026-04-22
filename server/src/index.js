5bbe4015b2cda165d7bd4a97609dc8827a395d77
labanie5 <labanie5@gmail.com>
2026-04-22 05:47:42 +0100

    Move drawing game server to port 3002 (3001 used by agentflow)

--- server/src/index.js
+++ server/src/index.js
@@ -139,5 +139,5 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
